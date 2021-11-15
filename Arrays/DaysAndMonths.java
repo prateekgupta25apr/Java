1a331df54569dbfc43feb80d0ad1eb5193051939
Prateek Gupta <prateek.gupta25apr@gmail.com>
2021-11-16 01:18:23 +0530

    Updated DaysAndMonths in Arrays

--- Arrays/DaysAndMonths.java
+++ Arrays/DaysAndMonths.java
@@ -1,12 +1,16 @@
 package Arrays;
 
-class DaysAndMonths{
-	public static void main(String []args){
-		String[] days={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",};
-		byte daysIndex=7;
-		String []months={"January","February","March","April","May","June","July","August","September","November","December",};
-		byte monthsIndex=4;
-		System.out.println(days[daysIndex-1]);
-		System.out.println(months[monthsIndex-1]);
-	}
+
+class DaysAndMonths {
+    public static void main(String[] args) {
+        String[] days = {
+                "Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday",};
+        byte daysIndex = 7;
+        String[] months = {
+                "January", "February", "March", "April", "May", "June", "July", "August",
+                "September", "November", "December",};
+        byte monthsIndex = 4;
+        System.out.println(days[daysIndex - 1]);
+        System.out.println(months[monthsIndex - 1]);
+    }
 }
