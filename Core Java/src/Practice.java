611d3b242540469be3abd8451d2521b139d4abb2
Prateek Gupta <prateek.gupta25apr@gmail.com>
2024-09-09 00:23:00 +0530

    Added MergeSort.java

--- Core Java/src/Practice.java
+++ Core Java/src/Practice.java
@@ -4,20 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Practice {
-    public static void main(String[] args) {
-        String[] array = {"A", "B", "C"};
-        List<String> list = Arrays.asList(array);
 
-// Modifying the array affects the list
-        array[0] = "X";
-        System.out.println(list);  // Output: [X, B, C]
-
-// Modifying the list affects the array
-        list.set(1, "Y");
-        System.out.println(Arrays.toString(array));  // Output: [X, Y, C]
-
-        list.add("P");
-
-    }
 }
 
